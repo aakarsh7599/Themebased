1edd6e69c739e0658f8e596cf1d5e2fa446cbaaa
aakarsh7599 <32263537+aakarsh7599@users.noreply.github.com>
2019-01-20 16:44:58 +0530

    Update PostListItem.jsx

--- src/Components/Feed/PostList/PostListItem.jsx
+++ src/Components/Feed/PostList/PostListItem.jsx
@@ -6,33 +6,33 @@ class PostListItem extends Component {
   render() {
     const {post} = this.props
     return (
-      <Segment.Group>
-        <Segment>
+      <Segment.Group raised>
+        <Segment raised>
           <Item.Group>
             <Item>
               <Item.Image size="tiny" circular src={post.postPhotoURL} />
               <Item.Content>
                 <Item.Header as="a">{post.title}</Item.Header>
                 <Item.Description>
-                  Posted by <a>{post.postedBy}</a><span> || </span>  
-                  <Icon name="clock" /> {post.date}<span> || </span>
-                  <Button size="mini" disabled content="Java" /> <Button size="mini" disabled content="Tech" />
+                  Posted by <a>{post.postedBy}</a><span> | </span> 
+                  <Icon name="code branch" />{post.category}<span> | </span>
+                  <Icon name="tags" /><Button size="mini" disabled content="Java" /> <Button size="mini" disabled content="Tech" />
                 </Item.Description>
               </Item.Content>
             </Item>
           </Item.Group>
         </Segment>
-        <Segment secondary>
+        <Segment raised secondary>
           <List horizontal>
-          {post.followers.map((follower) => (
+          {post.followers && post.followers.map((follower) => (
             <PostListFollower key={follower.id} follower={follower}/>
           ))}
 
           </List>
         </Segment>
-        <Segment clearing>
+        <Segment raised clearing>
         <span>{post.description}</span>
-          <Button as="a" color="teal" floated="right" content="View" />
+          <Button as="a" style={{background: '#00c07f'}} inverted floated="right" content="View" />
         </Segment>
       </Segment.Group>
     );
