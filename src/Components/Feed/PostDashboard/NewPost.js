dbf27f4bf82841a391d7314cd6e9fd6489d61cca
aakarsh7599 <32263537+aakarsh7599@users.noreply.github.com>
2019-01-20 16:45:57 +0530

    Update NewPost.js

--- src/Components/Feed/PostDashboard/NewPost.js
+++ src/Components/Feed/PostDashboard/NewPost.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Modal from 'react-awesome-modal';
-import { Button, Form, TextArea, Responsive,Dropdown } from 'semantic-ui-react';
+import { Input,Button, Grid,Form, TextArea, Responsive,Dropdown,Select, Segment } from 'semantic-ui-react';
 import './NewPost.css';
 
 class NewPost extends Component {
+   
+
     constructor(props) {
         super(props);
         this.state = {
-            visible : false
+            visible : false,
+            post: {
+                      title: '',
+                      description: '',
+                      category: '',
+                      postedBy: ''
+                    }
         }
     }
 
@@ -23,7 +31,23 @@ class NewPost extends Component {
         });
     }
 
+    onFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.createPost(this.state.post)
+  }
+
+  onInputChange = (event) => {
+    const newPost = this.state.post;
+    newPost[event.target.name] = event.target.value;
+    this.setState({
+      post: newPost
+    })
+  }
+
+  
+
     render() {
+        const {post} = this.state;
         const styling = {
             position: "right",
         };
@@ -32,31 +56,48 @@ class NewPost extends Component {
                              { key: 'javascript', value: 'javascript', text: 'JavaScript' }]
         return (
             <section>
-               {/* <h1>React-Modal Examples</h1>*/}
-                <button class="ui blue align inverted button" onClick={() => this.openModal()}>NewPost</button>                
+                <button style={{background: '#00c07f'}} class="ui inverted align button" onClick={() => this.openModal()}>NewPost</button>                
                 <Modal 
                     visible={this.state.visible}
                     width="800"
                     height="600"
                     effect="fadeInUp"
                     onClickAway={() => this.closeModal()}
-                >
+                >   <Segment raised inverted style={{background: '#313d4b'}}>
                     <div>
                         <div class="heading"><h1>New Post</h1></div>
-                        <div class="adjust">
+                        <Grid style={{background: '#313d4b'}}>
+                        <Grid.Column width={1}>
+                        </Grid.Column>
+                        <Grid.Column width={12}>
+                        <Segment raised inverted style={{background: '#313d4b'}}>
+                        <Form onSubmit={this.onFormSubmit}>
+                        <Form.Field>
+                        <Input name='title' onChange={this.onInputChange} placeholder='Post Title' />
+                        </Form.Field>
+                        <Form.Field>
+                        <TextArea name='description' onChange={this.onInputChange} placeholder='Post Description' style={{ minHeight: 200 }} />
+                        </Form.Field>
+                        <Form.Field>
+                        <Dropdown placeholder='Add Tags' multiple search selection options={tagOptions} />
+                        </Form.Field>
+                        <Form.Field><Select placeholder='Category' /></Form.Field>
+                        <Form.Field><Input name='postedBy' placeholder='Posted by' /></Form.Field>
+                        <Form.Field>
                         <Button.Group>
-                            <Button positive>Post</Button>
+                            <Button positive type="submit">Post</Button>
                             <Button.Or text='or' />
                             <Button onClick={() => this.closeModal()}>cancel</Button>
                         </Button.Group>
-                        </div>
-                        <Form>
-                        <div class='tarea'>
-                        <TextArea placeholder='Try adding multiple lines' style={{ minHeight: 400 }} />
-                        </div>
+                        </Form.Field>
                         </Form>
-                        <Dropdown placeholder='Tags' fluid multiple search selection options={tagOptions} />
+                        </Segment>
+                        </Grid.Column>
+                        <Grid.Column width={3}>
+                        </Grid.Column>
+                        </Grid>
                     </div>
+                    </Segment>
                 </Modal>
             </section>
         );
